73a2d72cf793026db44bd5a9a8ce7af6598a6bd4
Canisback <canisback@gmail.com>
2018-10-18 17:58:40 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'static-data',
+  packages = find_packages(),
+  version = '0.1.0',
+  description = 'Library to easily handles ddragon ressources files for League of Legends',
+  author = 'Canisback',
+  author_email = 'canisback@gmail.com',
+  url = 'https://github.com/Canisback/static-data',
+  keywords = ['Riot API', 'python','static-data'],
+  classifiers = [],
+  install_requires=[
+    "requests",
+    "enum"
+  ],
+)
