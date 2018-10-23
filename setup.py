092ade35485a71001c3f0abf08a90c5d5a01a9a8
Canisback <canisback@gmail.com>
2018-10-23 18:26:41 +0200

    Update to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'static_data',
   packages = find_packages(),
-  version = '0.1.0',
+  version = '0.2.0',
   description = 'Library to easily handles ddragon ressources files for League of Legends',
   author = 'Canisback',
   author_email = 'canisback@gmail.com',
